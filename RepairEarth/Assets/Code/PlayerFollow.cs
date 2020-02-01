abb00e2fe6b1217baa17708b623287efb4c06177
ile123ile <bbravencofounder@gmail.com>
2020-02-01 17:13:18 -0500

    made camera properly follow player

---
+++ RepairEarth/Assets/Code/PlayerFollow.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerFollow : MonoBehaviour
+{
+    public Transform player;
+    private float startX;
+    private float playerStartX;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startX = transform.position.x;
+        playerStartX = player.position.x; 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 newPos = transform.position;
+        newPos.x = startX + player.position.x - playerStartX;
+        transform.position = newPos;
+    }
+}
