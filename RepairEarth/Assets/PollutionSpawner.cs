93db345b911628f8c7750727d21ee042f50930ba
ile123ile <bbravencofounder@gmail.com>
2020-02-01 18:09:09 -0500

    made trash spawn automatically

---
+++ RepairEarth/Assets/PollutionSpawner.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PollutionSpawner : MonoBehaviour
+{
+    public GameObject trash;
+    public string spawnTag = "PollutionSpot";
+
+    public float spawnTime = 1f;
+
+    private float timeToSpawn;
+    private GameObject[] spawnLocs;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        spawnLocs = GameObject.FindGameObjectsWithTag(spawnTag);
+        foreach (GameObject obj in spawnLocs)
+        {
+            obj.SetActive(false);
+        }
+        timeToSpawn = spawnTime;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeToSpawn -= Time.deltaTime;
+        if (timeToSpawn <= 0)
+        {
+            timeToSpawn = spawnTime;
+            SpawnTrash();
+        }
+    }
+
+    void SpawnTrash()
+    {
+        int n = spawnLocs.Length;
+        while (n > 1)
+        {
+            int k = Random.Range(0,n--);
+            GameObject temp = spawnLocs[n];
+            spawnLocs[n] = spawnLocs[k];
+            spawnLocs[k] = temp;
+        }
+        foreach (GameObject obj in spawnLocs)
+        {
+            if (!obj.activeSelf)
+            {
+                obj.SetActive(true);
+                Debug.Log("Cow");
+                return;
+            }
+        }
+    }
+}
