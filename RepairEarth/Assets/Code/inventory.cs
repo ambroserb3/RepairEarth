98c2da10fc05201b9ab2a546fc9e860634110360
stephanrb3 <stephanrb3@gmail.com>
2020-02-01 15:12:35 -0500

    collectibles
    collectibles

---
+++ RepairEarth/Assets/Code/inventory.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class inventory : MonoBehaviour
+{
+    public bool item1 = false;
+    public bool item2 = false;
+    public bool item3 = false;
+    public bool item4 = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (item1 == true)
+        {
+            //can do something new
+        }
+        if (item2 == true)
+        {
+            //can do something new
+        }
+        if (item3 == true)
+        {
+            //can do something new
+        }
+        if (item4 == true)
+        {
+            //can do something new
+        }
+    }
+}
