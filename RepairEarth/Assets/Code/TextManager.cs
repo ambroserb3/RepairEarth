5f61cfdeb88d3ce497e8c3c87f7587dc32a029a4
ile123ile <bbravencofounder@gmail.com>
2020-02-01 16:19:23 -0500

    added serialization capability to text manager

--- RepairEarth/Assets/Code/TextManager.cs
+++ RepairEarth/Assets/Code/TextManager.cs
@@ -1,17 +1,32 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
+[System.Serializable]
+public class StoryLine
+{
+    public string line;
+}
 
 public class TextManager : MonoBehaviour
 {
+
+    public StoryLine[] story;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
+    {
+        
+    }
+
+    void SetText()
     {
         
     }
