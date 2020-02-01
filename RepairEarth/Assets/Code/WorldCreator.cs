94ccc3497e71d304006553e67d07e8662c1a5fb4
Benjamin Most <benjaminmost@gmail.com>
2020-02-01 16:19:30 -0500

    Started making WorldBuilder object to generate world

---
+++ RepairEarth/Assets/Code/WorldCreator.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorldCreator : MonoBehaviour
+{
+    public GameObject dirtGround;
+    private Vector3 startLoc = new Vector3(-100, -1.7f, 2);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Instantiate(dirtGround, startLoc, Quaternion.identity);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
